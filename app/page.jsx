077800cb3ad2a64e4b55ec00f399b1e3a5248cab
Xun Ruan <xunruan@icloud.com>
2023-07-17 15:40:48 -0400

    Test

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+
+import '@styles/globals.css'
+import Example from '@components/offset-two-times-two-grid.jsx'
+
+export default function Home() {
+    return (
+        <div>
+            <Example/>
+        </div>
+    )
+}
